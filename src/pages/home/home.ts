52d07a63d812233f63a498df6f0a9088cad25374
OUMAYMA_BOURICH <36383216+bourichoumayma@users.noreply.github.com>
2020-01-04 18:37:33 +0100

    Create home.ts

---
+++ src/pages/home/home.ts
@@ -0,0 +1,57 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Subscription } from 'rxjs/Subscription';
+import { filter } from 'rxjs/operators';
+import { Storage } from '@ionic/storage';
+ 
+declare var google;
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  currentMapTrack = null;
+ 
+  isTracking = false;
+  trackedRoute = [];
+  previousTracks = [];
+ 
+  positionSubscription: Subscription;
+ 
+  constructor(public navCtrl: NavController, private plt: Platform, private geolocation: Geolocation, private storage: Storage) { }
+ 
+  ionViewDidLoad() {
+    this.plt.ready().then(() => {
+      this.loadHistoricRoutes();
+ 
+      let mapOptions = {
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+      this.geolocation.getCurrentPosition().then(pos => {
+        let latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        this.map.setCenter(latLng);
+        this.map.setZoom(16);
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    });
+  }
+ 
+  loadHistoricRoutes() {
+    this.storage.get('routes').then(data => {
+      if (data) {
+        this.previousTracks = data;
+      }
+    });
+  }
+}
